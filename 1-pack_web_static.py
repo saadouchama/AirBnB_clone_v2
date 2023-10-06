12e47fa00c1119d0b06c241c60045eb690612002
saadochama <saadochama@gmail.com>
2023-10-06 12:42:56 +0000

    add mandatory and advanced tasks

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,22 +1,19 @@
 #!/usr/bin/python3
+""" module doc
 """
-Fabric script that generates a tgz archive from the contents of the web_static
-folder of the AirBnB Clone repo
-"""
-
+from fabric.api import task, local
 from datetime import datetime
-from fabric.api import local
-from os.path import isdir
 
 
+@task
 def do_pack():
-    """generates a tgz archive"""
-    try:
-        date = datetime.now().strftime("%Y%m%d%H%M%S")
-        if isdir("versions") is False:
-            local("mkdir versions")
-        file_name = "versions/web_static_{}.tgz".format(date)
-        local("tar -cvzf {} web_static".format(file_name))
-        return file_name
-    except:
-        return None
+    """ method doc
+        sudo fab -f 1-pack_web_static.py do_pack
+    """
+    formatted_dt = datetime.now().strftime('%Y%m%d%H%M%S')
+    mkdir = "mkdir -p versions"
+    path = "versions/web_static_{}.tgz".format(formatted_dt)
+    print("Packing web_static to {}".format(path))
+    if local("{} && tar -cvzf {} web_static".format(mkdir, path)).succeeded:
+        return path
+    return None
